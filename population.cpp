2eb7f61b7360f79699dce5f7cd349557b73d5224
Adrian Smith <147835901+adismith2023@users.noreply.github.com>
2023-12-10 19:31:45 +0000

    Add files via upload

---
+++ population.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//#include <iomanip> // For std::fixed and std::setprecision
+//using namespace std;
+//
+//int main()
+//{
+//    // Initial world population
+//    long long worldPop = 7700000000LL;
+//
+//    // Print world population
+//    cout << "The world population is " << worldPop<< endl;
+//    // Calculate and print the population after increases of 10%, 15%, and 20%
+//    cout << "If the world population rose by 10% it would be " << fixed << setprecision(0) << worldPop * 1.10 << endl;
+//    cout << "If the world population rose by 15% it would be " << fixed << setprecision(0) << worldPop * 1.15 << endl;
+//    cout << "If the world population rose by 20% it would be " << fixed << setprecision(0) << worldPop * 1.20 << endl;
+//
+//    return 0;
+//}
